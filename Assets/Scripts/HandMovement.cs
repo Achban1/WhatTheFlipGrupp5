d694e73393e9bc4a928d5ea5c7516feb7cb39bc8
toveandersson <toveama@gmail.com>
2023-10-23 22:56:13 +0200

    hand

--- Assets/Scripts/HandMovement.cs
+++ Assets/Scripts/HandMovement.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HandMovement : MonoBehaviour
 {
     public float rotationSpeed = 60f;
-    //private Vector3 currentPosition;
     private float rotationInput;
     private float rotationAmount;
     float smooth = 5.0f;
-    float min = -1710;
-    float max = 1710;
+    float min = 90;
+    float max = -90;
+    float timer = 0f;
 
     private void Start()
     {
@@ -19,22 +20,20 @@ public class HandMovement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetAxis("HorizontalPlayer1") == 0)
+        float zValue = Input.GetAxis("HorizontalPlayer1");
+        if (zValue == 0)
         {
             Debug.Log("get to 0");
-            transform.position = new Vector3(0,0,0);
+            timer = 0f;
         }
-        float xPos = Mathf.Clamp(xValue, min, max);
-
-        //float tiltAroundZ = Input.GetAxis("Horizontal") * rotationSpeed;
-        //Quaternion target = Quaternion.Euler(0, 0, tiltAroundZ);
-        //rotationAmount = Mathf.Lerp(-1710, 1710, rotationInput);
-
-        //transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
-        //rotationInput = Input.GetAxis("HorizontalPlayer1") * rotationSpeed * Time.deltaTime;
-        //Quaternion hej = new Quaternion(0, 0, rotationAmount, 0);
-        //Transform.Rotate()
-        //1710 är max
+        else
+        {
+            float clampedValue = Mathf.Clamp(zValue, -1f, 1f); // Adjust the range of the input
+            float t = Mathf.PingPong(timer, 1f);
+            float zPos = Mathf.Lerp(min, max, (clampedValue + 1f) / 2f); // Map the clamped value to the range between 0 and 1
+            transform.rotation = Quaternion.Euler(0, 0, zPos);
+            timer += Time.deltaTime;
+        }
     }
 }
 
